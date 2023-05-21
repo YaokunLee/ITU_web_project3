295d13306f84b3a50ba456920090c4d82bf8a030
LiYaokun97 <nukoayil@qq.com>
2023-05-21 15:38:52 +0200

    [fix] login page

--- reactpages/src/login/LoginForm.js
+++ reactpages/src/login/LoginForm.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {onSubmitLogin} from "../data/login"
 
 function LoginForm() {
-    const onUserSubmitLogin = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onUserSubmitLogin = (event) => {
+        event.preventDefault();
         // 处理登录表单提交的逻辑
-        onSubmitLogin()
+        onSubmitLogin(username, password);
     };
 
     return (
@@ -12,12 +16,24 @@ function LoginForm() {
             <br />
             <br />
             <div className="container-fluid">
-                <form className="loginframe">
+                <form className="loginframe" onSubmit={onUserSubmitLogin}>
                     <label className="account">Name:</label>
-                    <input className="form-input" id="account" type="text" value="" /><br/><p></p>
+                    <input
+                        className="form-input"
+                        id="account"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    /><br/><p></p>
                     <label className="password">Password:</label>
-                    <input className="form-input" id="password" type="password" value="" /><br/><br/>
-                    <input className="btn-submit" type="button" onClick={onUserSubmitLogin} value="SUBMIT" />
+                    <input
+                        className="form-input"
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    /><br/><br/>
+                    <input className="btn-submit" type="submit" value="SUBMIT" />
                 </form>
             </div>
         </div>
